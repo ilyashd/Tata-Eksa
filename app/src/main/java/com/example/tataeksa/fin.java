6c289229867fffba26d46cdd8379c36279e45848
doniantoro <doniantoro34@gmail.com>
2020-01-09 19:36:50 +0700

    addd

--- app/src/main/java/com/example/tataeksa/fin.java
+++ app/src/main/java/com/example/tataeksa/fin.java
@@ -1,11 +1,9 @@
 package com.example.tataeksa;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class fin extends AppCompatActivity {
@@ -17,6 +15,7 @@ public class fin extends AppCompatActivity {
 
         final TextView txt= (TextView) findViewById(R.id.textView21);
         final TextView txt2= (TextView) findViewById(R.id.textView22);
+
         txt.setText(getIntent().getStringExtra("nama"));
         txt2.setText(getIntent().getStringExtra("phone"));
 
@@ -27,8 +26,7 @@ public class fin extends AppCompatActivity {
             public void onClick(View view) {
                 Intent int4 = new Intent(fin.this,MainActivity.class);
                 startActivity(int4);
-
             }
             });
-    }
+        }
 }
